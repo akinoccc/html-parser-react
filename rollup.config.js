c738b303012cc2b38304cb008a514dc71c1dc7b4
amschen <amschen@tencent.com>
2022-03-27 17:03:28 +0800

    add: add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+/*
+ * rollup 配置文件
+ */
+
+export default {
+  input: 'index.js',
+  output: {
+    dir: 'npm',
+    format: 'cjs',
+    sourcemap: true,
+    exports: 'named'
+  },
+  //告诉rollup不要将此打包，而作为外部依赖
+  external: ['react', 'lib', ''],
+  // 是否开启代码分割
+  experimentalCodeSplitting: false,
+  plugins: []
+};
