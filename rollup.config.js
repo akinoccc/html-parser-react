2223576ad62dfbb85d7b55257af4ee660289ee05
amschen <amschen@tencent.com>
2022-03-27 17:15:07 +0800

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,3 @@
-/*
- * rollup 配置文件
- */
-
 export default {
   input: 'index.js',
   output: {
@@ -10,9 +6,7 @@ export default {
     sourcemap: true,
     exports: 'named'
   },
-  //告诉rollup不要将此打包，而作为外部依赖
   external: ['react', 'lib', ''],
-  // 是否开启代码分割
   experimentalCodeSplitting: false,
   plugins: []
 };
