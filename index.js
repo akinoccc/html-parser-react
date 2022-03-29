fcbf1b60a5a3ceda73d3e0aff0bc6418a0a1db31
amschen <amschen@tencent.com>
2022-03-29 23:13:11 +0800

    style: add functions' description

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import transformToHtmlElement from './lib/transformToHtmlElement';
 import transformToReactElement from './lib/transformToReactElement';
 
+/**
+ * @name: AmsChan
+ * @param {string} htmlStr
+ * @param {object} data
+ * @param {object} option
+ * @return {JSX.Element[]} ReactElement[]
+ */
 const Parser = ({ htmlStr, data, option }) => {
   const htmlDom = transformToHtmlElement(htmlStr, data);
   const reactDom = transformToReactElement(htmlDom, option);
